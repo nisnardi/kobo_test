8823562cff1515d21af438c2f1052b480113a74f
Nicolas Isnardi <nicolasisnardi@gmail.com>
2025-02-22 05:44:22 -0500

    * 2) Convert Question1.tsx class component to a functional component

--- app/Question1.tsx
+++ app/Question1.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
@@ -7,25 +7,34 @@ import {
   Image,
   TouchableOpacity,
   SafeAreaView,
-} from 'react-native';
+} from "react-native";
 
-export default class Question1 extends Component {
-  _onPressItem = () => {
-    console.log('open item');
+export default function Question1() {
+  const _onPressItem = () => {
+    console.log("open item");
   };
 
-  renderItem({ item, index }) {
+  const renderItem = ({ item }) => {
     return (
-      <TouchableOpacity onPress={this._onPressItem}>
-        <View style={{ flex: 1, flexDirection: 'row', backgroundColor: item.backgroundColor }}>
-          <Image style={{ width: 100, height: 100 }} source={{ uri: item.avatar }} />
+      <TouchableOpacity onPress={_onPressItem}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            backgroundColor: item.backgroundColor,
+          }}
+        >
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: item.avatar }}
+          />
           <View
             style={{
               flex: 1,
               paddingLeft: 10,
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'stretch',
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "stretch",
             }}
           >
             <Text>
@@ -36,74 +45,80 @@ export default class Question1 extends Component {
         </View>
       </TouchableOpacity>
     );
-  }
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <SafeAreaView>
-          <View
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <View
+          style={{
+            height: 60,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text
             style={{
-              height: 60,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
+              textAlign: "center",
+              fontSize: 20,
+              fontWeight: "bold",
+              paddingTop: 20,
             }}
           >
-            <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold', paddingTop: 20 }}>
-              User List
-            </Text>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}
-            >
-              {/* // Grid mode */}
-              <Image
-                style={{ width: 30, height: 10 }}
-                source={require('../assets/images/grid.png')}
-              />
-              {/* // List mode */}
-              <Image
-                style={{ width: 30, height: 30 }}
-                source={require('../assets/images/list.png')}
-              />
-              {/* // Sort last Name A-Z */}
-              <Image
-                style={{ width: 30, height: 30 }}
-                source={require('../assets/images/sort_az.png')}
-              />
-              {/* // Sort last Name Z-A */}
-              <Image
-                style={{ width: 30, height: 30 }}
-                source={require('../assets/images/sort_za.png')}
-              />
-              {/* // Only show elements that have large avatars */}
-              <Image
-                style={{ width: 30, height: 30 }}
-                source={require('../assets/images/avatar.png')}
-              />
-            </View>
+            User List
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* // Grid mode */}
+            <Image
+              style={{ width: 30, height: 10 }}
+              source={require("../assets/images/grid.png")}
+            />
+            {/* // List mode */}
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={require("../assets/images/list.png")}
+            />
+            {/* // Sort last Name A-Z */}
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={require("../assets/images/sort_az.png")}
+            />
+            {/* // Sort last Name Z-A */}
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={require("../assets/images/sort_za.png")}
+            />
+            {/* // Only show elements that have large avatars */}
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={require("../assets/images/avatar.png")}
+            />
           </View>
-          <FlatList
-            contentContainerStyle={styles.list}
-            data={require('../assets/MOCK_DATA.json')}
-            renderItem={this.renderItem}
-          />
-        </SafeAreaView>
-      </View>
-    );
-  }
+        </View>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={require("../assets/MOCK_DATA.json")}
+          renderItem={renderItem}
+        />
+      </SafeAreaView>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: "#F5FCFF",
   },
   list: {
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   bar: {},
 });
